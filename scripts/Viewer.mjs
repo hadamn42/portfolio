eea4b06231f4617ecda379c97e27c8df5998f1fa
hadamn42 <68972061+hadamn42@users.noreply.github.com>
2025-07-23 22:33:54 -0600

    Added new piece and close button

--- scripts/Viewer.mjs
+++ scripts/Viewer.mjs
@@ -7,6 +7,7 @@ export class Viewer {
 function openDoc(){
     const docLink = document.querySelectorAll(".pdflink");
     const docViewer = document.getElementById("viewer");
+    const closeButton = document.getElementById("closer");
     const ul = document.getElementById("works");
     const description = document.querySelectorAll(".description");
 
@@ -16,6 +17,8 @@ function openDoc(){
 
             ul.id = "works-shown";
             docViewer.classList.replace("hide", "show");
+            closeButton.classList.replace("hide", "show");
+            
             description.forEach((p) => {
                 p.classList.add("hide");
             });
@@ -24,6 +27,8 @@ function openDoc(){
             const docRef = button.href
             
             docViewer.src = docRef;
+
+            closeBolton();
 
             const headsDesc = document.getElementById("welcome");
             headsDesc.innerHTML = "Welcome to my portfolio. Please click on a link to see some of my work. Hover on the link for a brief description.";
@@ -41,5 +46,36 @@ function createHover() {
         d.style.height = "69px";
         d.style.display = "grid";
         d.style.alignItems = "center";
+    });
+}
+
+function closeBolton() {
+    const docLink = document.querySelectorAll(".pdflink");
+    const docViewer = document.getElementById("viewer");
+    const closeButton = document.getElementById("closer");
+    const ul = document.getElementById("works-shown");
+    const description = document.querySelectorAll(".description");
+
+    closeButton.addEventListener("click", (event)=>{
+        ul.id = "works";
+        docViewer.classList.replace("show", "hide");
+        closeButton.classList.replace("show", "hide");
+        description.forEach((p) => {
+            p.classList.remove("hide");
+        });
+
+        const headsDesc = document.getElementById("welcome");
+        headsDesc.innerHTML = "Welcome to my portfolio. Please click on a link to see some of my work.";
+
+        const pieces = document.querySelectorAll(".portpieces");
+        pieces.forEach((d) => {
+            // const titleDesc = d.querySelector(".description");
+            // const info = titleDesc.innerHTML;
+            d.title = "";
+            d.style.textAlign = "";
+            d.style.height = "";
+            d.style.display = "";
+            d.style.alignItems = "";
+        });
     });
 }
